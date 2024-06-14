07ebb0009051fadfda1e79b2d2933d775c2d5c8f
Rohan Kapur <rohankapurdev@outlook.com>
2024-06-15 00:22:36 +0800

    feat: add v3 derivation

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::str::FromStr;
 
 // Constants
 const SQUADS_PROGRAM_ID: &str = "SQDS4ep65T869zMMBKyuUq6aD6EgTu8psMjkvj52pCf";
+const SQUADS_V3_PROGRAM_ID: &str = "SMPLecH534NA9acpos4G6x7uf3LWbCAwZQE9e8ZekMu";
 const SEED_PREFIX: &[u8] = b"multisig";
 const SEED_VAULT: &[u8] = b"vault";
 
@@ -17,6 +18,14 @@ fn main() {
         let vault_address = derive_vault_address(&multisig_address, i, &program_id);
         println!("Vault address {}: {}", i, vault_address);
     }
+
+    let v3_multisig_address =
+        Pubkey::from_str("6eEtYNMzxFiy5t1mvJmJZRTFFN1Fj6fXCYt5F1ty4PAL").unwrap();
+    let v3_program_id = Pubkey::from_str(SQUADS_V3_PROGRAM_ID).unwrap();
+
+    let v3_multisig_vault = derive_vault_address_v3(&v3_multisig_address, &v3_program_id);
+
+    println!("V3 multisig vault: {}", v3_multisig_vault);
 }
 
 /// This function derives a vault address from a multisig address and an index. In squads V4, the index 0 is always considered
@@ -28,6 +37,18 @@ fn derive_vault_address(multisig_address: &Pubkey, index: u8, program_id: &Pubke
         multisig_address.as_ref(),
         SEED_VAULT,
         &index.to_le_bytes(),
+    ];
+
+    let vault_address: (Pubkey, u8) = Pubkey::find_program_address(vault_seeds, program_id);
+    vault_address.0
+}
+
+fn derive_vault_address_v3(multisig_address: &Pubkey, program_id: &Pubkey) -> Pubkey {
+    let vault_seeds = &[
+        b"squad",
+        multisig_address.as_ref(),
+        &1_u32.to_le_bytes(),
+        b"authority",
     ];
 
     let vault_address: (Pubkey, u8) = Pubkey::find_program_address(vault_seeds, program_id);
